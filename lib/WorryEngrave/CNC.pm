8c02b11bcfc29dbb485cbf4e62c93ee10b38bdf6
James Stanley <james@incoherency.co.uk>
2021-03-21 11:31:52 +0000

    Line-buffer input; compute work position

--- lib/WorryEngrave/CNC.pm
+++ lib/WorryEngrave/CNC.pm
@@ -3,6 +3,17 @@ package WorryEngrave::CNC;
 my $CNC_FH;
 my $dryrun;
 
+# Grbl normally only reports Machine Position (MPos), but we need to know Work
+# Position (WPos). There is an option ($10) to enable reporting of WPos instead
+# of MPos but only intended for human use; MPos is recommended for software.
+#
+# Grbl also reports Work Coordinate Offset (WCO) whenever it changes. To get from
+# MPos to WPos, we just need to keep track of WCO, and then:
+#   WPos = MPos - WCO
+my @wco = (0,0,0);
+
+my $buf = '';
+
 sub dryrun {
     $dryrun = 1;
 }
@@ -19,20 +30,39 @@ sub connect {
     return open($CNC_FH, '+<', $dev)
 }
 
+# line-buffer input
+sub read_line {
+    my ($pkg) = @_;
+
+    while (<$CNC_FH>) {
+        $buf .= $_;
+        return $1 if $buf =~ s/^.*\n//;
+    }
+
+    return undef;
+}
+
+# return current work coordinates
 sub get_coords {
+    my ($pkg) = @_;
+
     return (undef,undef,undef) if $dryrun;
 
     print $CNC_FH "?\n";
 
     while (1) {
-        while (<$CNC_FH>) {
+        while ($_ = $pkg->read_line) {
             if (/WCO:\s*([-.0-9]+),([-.0-9]+),([-.0-9]+)/) {
-                return ($1,$2,$3);
+                @wco = ($1,$2,$3);
+            }
+            if (/MPos:\s*([-.0-9]+),([-.0-9]+),([-.0-9]+)/) {
+                return ($1-$wco[0],$2-$wco[1],$3-$wco[2]);
             }
        }
     }
 }
 
+# send a message to the controller and wait for it to respond (preferably with "ok" but we don't check for that)
 sub send {
     my ($pkg, $gcode) = @_;
 
@@ -42,7 +72,7 @@ sub send {
 
     my $gotline = 0;
     while (!$gotline) {
-        while (<$CNC_FH>) {
+        while ($_ = $pkg->read_line) {
             $gotline = 1;
        }
     }
